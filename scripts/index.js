0d9ad394d8374c4201e0878010d291e21c16dd4f
Lyashyk <aleks1002x@gmail.com>
2019-02-09 15:35:56 +0200

    new commit

--- scripts/index.js
+++ scripts/index.js
@@ -18,15 +18,18 @@ window.addEventListener("scroll", onscrollEventFunction);
 
 //----------------------------------------------------------------------------------------------------------------------
 const buttonMainMenu = document.getElementsByClassName("burgerButton")[0];
+const mainMenu = document.getElementsByClassName("mainMenu")[0];
 var buttonMainMenuClickEventFunctionFlag = 1;
 
 buttonMainMenuClickEventFunction = () => {
   if (buttonMainMenuClickEventFunctionFlag === 1) {
     buttonMainMenu.classList.add("burgerButton--click");
     buttonMainMenu.classList.remove("burgerButton--unclick");
+    mainMenu.classList.add("mainMenu--visibleList");
   } else {
     buttonMainMenu.classList.remove("burgerButton--click");
     buttonMainMenu.classList.add("burgerButton--unclick");
+    mainMenu.classList.remove("mainMenu--visibleList");
   }
   // return (buttonMainMenuClickEventFunctionFlag = -buttonMainMenuClickEventFunctionFlag);
   buttonMainMenuClickEventFunctionFlag = -buttonMainMenuClickEventFunctionFlag;
